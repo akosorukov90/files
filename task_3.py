ac133cc4890fd84a79649a180085945a218f7193
Aleksandr Kosorukov <akosorukov90@gmail.com>
2020-10-05 22:41:30 +0300

    used itemgetter in task_3

--- task_3.py
+++ task_3.py
@@ -1,48 +1,35 @@
 import os
+from operator import itemgetter
 
 
-class File:
-    all_files = []
-
-    def __init__(self, name, path):
-        self.name = name
-        self.path = path
-        self.count = 0
-        self.all_files.append(self)
-
-    def get_count_str(self):
-        count = 0
-        with open(self.path, 'r', encoding='utf-8') as f:
-            for line in f:
-                count += 1
-        self.count = count
-
-
-def glue_files():
-    all_count_str = []
-    for file in File.all_files:
-        all_count_str.append(file.count)
-    all_count_str.sort()
+def glue_files(file_list):
+    all_files = file_name_path_count(file_list)
     file_path_result = os.path.join(os.getcwd(), 'result.txt')
     with open(file_path_result, 'w', encoding='utf-8') as f_write:
-        for count in all_count_str:
-            for value in File.all_files:
-                if value.count == count:
-                    f_write.write(value.name + '\n')
-                    f_write.write(str(value.count) + '\n')
-                    with open(value.path, 'r', encoding='utf-8') as f_read:
-                        for line in f_read:
-                            f_write.write(line)
+        for file in all_files:
+            f_write.write(file[0] + '\n')
+            f_write.write(str(file[2]) + '\n')
+            with open(file[1], 'r', encoding='utf-8') as f_read:
+                for line in f_read:
+                    f_write.write(line)
 
 
-def file_name_path(file_list):
-    for file in files:
+def get_count_str(path):
+    count = 0
+    with open(path, 'r', encoding='utf-8') as f:
+        for line in f:
+            count += 1
+    return count
+
+
+def file_name_path_count(file_list):
+    all_files = []
+    for file in file_list:
         file_path = os.path.join(os.getcwd(), file)
-        file_new = File(file, file_path)
-        for file in File.all_files:
-            file.get_count_str()
+        count_str = get_count_str(file_path)
+        all_files.append((file, file_path, count_str))
+    return sorted(all_files, key=itemgetter(2))
 
 
 files = ['1.txt', '2.txt', '3.txt']
-file_name_path(files)
-glue_files()
+glue_files(files)
